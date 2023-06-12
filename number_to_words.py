9c84cd6d748821ae0ece9ba20c171e191af55440
Nehal Vaghasiya <nehal.vaghasiya@sttech.de>
2023-06-12 12:07:35 +0200

    num2words added

---
+++ number_to_words.py
@@ -0,0 +1,16 @@
+# pip install num2words
+
+
+from num2words import num2words
+
+print(num2words(2019)) # 'two thousand and nineteen'
+
+print(num2words(2019, to='ordinal')) # 'two thousand and nineteenth'
+
+print(num2words(2019, to='ordinal_num')) # '2019th'
+
+print(num2words(2019, to='year')) # 'twenty nineteen'
+
+# generate ordinal numbers and support multiple languages
+
+print(num2words(2019, lang='de')) # zweitausendneunzehn
